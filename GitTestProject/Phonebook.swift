80c0c0781173ff68c8b012223a0d289631be47ff
Davide D'Andrea <davide@deboscio.fritz.box>
2017-12-15 00:17:55 +0100

    add phonebook feature (for kidding!)

---
+++ GitTestProject/Phonebook.swift
@@ -0,0 +1,13 @@
+//
+//  Phonebook.swift
+//  GitTestProject
+//
+//  Created by Davide D'Andrea on 15/12/2017.
+//  Copyright © 2017 Davide D'Andrea. All rights reserved.
+//
+
+import UIKit
+
+class Phonebook: NSObject {
+
+}
